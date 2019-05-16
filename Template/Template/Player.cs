9aaec021c9b0c6c86e004d5303de6f2a54238fb2
Alex Frier Stewart <alex7175@student.eadania.dk>
2019-05-16 10:44:56 +0200

    update

---
+++ Template/Template/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Template
+{
+    public class Player : GameObject
+    {
+        public Player(string spriteName, Transform Transform) : base(spriteName, Transform)
+        {
+        }
+    }
+}
